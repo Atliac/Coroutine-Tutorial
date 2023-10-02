69f34f1e0c4636115865c2baef344ae2292fff29
Atliac <Atliac@outlook.com>
2023-10-03 02:21:30 +0800

    Add hello_coroutine example

--- hello_coroutine/main.cpp
+++ hello_coroutine/main.cpp
@@ -1,7 +1,27 @@
 ﻿#include <iostream>
 #include <coroutine>
 
+using namespace std;
+template<> struct std::coroutine_traits<void>
+{
+    struct promise_type
+    {
+        void get_return_object() noexcept {}
+        auto initial_suspend() noexcept { return std::suspend_never{}; }
+        auto final_suspend() noexcept { return std::suspend_never{}; }
+        void return_void() noexcept {}
+        void unhandled_exception() noexcept {}
+    };
+};
+
+void hello_coroutine()
+{
+    cout << "Hello coroutine" << endl;
+    co_return;
+}
+
 int main()
 {
+    hello_coroutine();
 	return 0;
 }
